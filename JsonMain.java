43333dfd36c4b353b05130cea7afc54869501b6e
tneeraja <tneeraja95@gmail.com>
2017-07-26 12:44:22 +0530

    grammer includes array but on strings only

--- JsonMain.java
+++ JsonMain.java
@@ -1,7 +1,8 @@
+import java.io.*;
 class JsonParser{
 	int i;
 	String s;
-	boolean match(char c){
+	boolean Match(char c){
 		if(s.charAt(i)==' ')
 			i++;
 		if(s.charAt(i)==c){
@@ -12,16 +13,16 @@ class JsonParser{
 	}
 	boolean Object(){
 		if(s.charAt(i)=='{' || s.charAt(i)==' '){
-			if(match('{')){
+			if(Match('{')){
 				if(s.charAt(i)=='}' || s.charAt(i)==' '){
-					match('}');
+					Match('}');
 					return true;
 				}		
 				else{
 					System.out.println("inside else");
 					if(Members()){
 						System.out.println("inside Object members");
-						return(match('}'));
+						return(Match('}'));
 					}
 				}			
 			}	 	
@@ -31,7 +32,15 @@ class JsonParser{
 	
 	boolean Members(){
 					System.out.println("inside members");
-		return Pair();
+		if(Pair()){
+			if(s.charAt(i)==',' || s.charAt(i)==' ') {
+				Match(',');
+				return Members(); 	
+			}
+			else
+				return true;
+		}
+		return false;
 	}
 	
 	boolean Pair(){
@@ -40,53 +49,91 @@ class JsonParser{
 					System.out.println("inside pair string"+s.charAt(i));
 			if(s.charAt(i)==':'|| s.charAt(i)==' '){
 					System.out.println("inside pair :");
-				if(match(':'))
-					return value();
+				if(Match(':'))
+					return Value();
 			}			
 		}	
 		
 		return false;
 	}
-	boolean value(){
+	boolean Value(){
 		return string();
 	}
 	boolean string(){
 		if(s.charAt(i)=='"'|| s.charAt(i)==' '){
-			if(match('"')){
-				if(chars())
-					return match('"');
+			if(Match('"')){
+				if(Chars())
+					return Match('"');
 			}
 		}	
 		return false;
 	}
 	
-	boolean chars(){
+	boolean Chars(){
 				System.out.println("inside chars");
 		if(s.charAt(i)=='"'|| s.charAt(i)==' ')
 			return true;
-		if(character())
-			return chars();
+		if(Character())
+			return Chars();
 		return false;	
 	}
-	boolean character(){
+	boolean Character(){
 		if(s.charAt(i)>='A'&& s.charAt(i)<='Z'){
 			i++;
 			return true;
 		}	
 		return false;	
 	}
-	public void validator(String s){
+	
+	boolean Array(){
+		if(s.charAt(i)=='['){
+			System.out.println("Inside array if");
+			Match('[');
+			if(s.charAt(i)==']')
+				return(Match(']'));
+			else
+				if(Elements())
+					return Match(']');	
+		}
+		return false;
+	}
+	
+	boolean Elements(){
+		boolean flag;
+		flag=Value();
+		if(flag==false)
+			return flag;
+		if(s.charAt(i)==',' || s.charAt(i)==' '){
+			Match(',');
+			return Elements();
+		}
+		return flag;	
+	}
+	public void Validator(String s){
 		i=0;
 		this.s=s;
-		if(Object())
+		boolean flag=false;
+		if(s.charAt(i)==' ')
+			i++;
+		if(s.charAt(i)=='{'){
+			if(Object())
+				flag=true;
+			}
+		else if(s.charAt(i)=='['){
+			if(Array())
+				flag=true;
+		}	
+		if(flag==true)
+		System.out.println("returned true i="+i+" length="+s.length());
+		if(i<s.length() || flag==false)
+			System.out.println("String is not valid");	
+		else 
 			System.out.println("String is valid");
-		else
-			System.out.println("String is not valid");
 	}	
 }
 class JsonMain{
 	public static void main(String[] args){
 			JsonParser JP = new JsonParser();
-			JP.validator("{\" AAA \" :\"SQQQ\"}");		
+			JP.Validator(" {\"AAA\":\"QQQ\" , \"WW\":\"WW\" , \"\":\"QQ\"}");		
 	}	
 }
